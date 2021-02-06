a3e535d1ce145fedaa3beeea1378ddafae4b08db
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-06 05:53:30 -0500

    btn contact

---
+++ src/components/BtnContact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import "../styles/btn.css";
+
+function BtnContact() {
+  return (
+    <div className="btn">
+      <Button>
+        <a
+          href="https://drive.google.com/file/d/11azypBy1lPKw4ZzdWhGc793nOVDDImy2/view?usp=sharing"
+          download="cv-jael-rr"
+        >
+          Let's talk
+        </a>
+      </Button>
+    </div>
+  );
+}
+
+export default BtnContact;

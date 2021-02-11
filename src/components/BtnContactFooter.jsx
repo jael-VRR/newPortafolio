6d9f3d74bce9507a397d44568be1e7c573383e84
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-11 17:31:54 -0500

    modifying color button footer

--- src/components/BtnContactFooter.jsx
+++ src/components/BtnContactFooter.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Button } from "react-bootstrap";
 import "../styles/btnFooter.css";
 
 function BtnContactFooter() {
     return (
         <div className="btn-footer">
-        <Button>
+        <button className="btn-final" >
           <a className="text-btn"
             href="https://drive.google.com/file/d/11azypBy1lPKw4ZzdWhGc793nOVDDImy2/view?usp=sharing"
             download="cv-jael-rr"
           >
             Let's talk!
           </a>
-        </Button>
+        </button>
       </div>
     )
 }

e0ec808be8ff903f45101e7b1b8d1353b7f48107
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-11 17:34:44 -0500

    position icons

--- src/components/SectionTools.jsx
+++ src/components/SectionTools.jsx
@@ -8,17 +8,92 @@ function SectionTools() {
       <Row>
         <Col className="home-services">
           <div className="container-text">
-            <h2>What We Do</h2>
+            <h2>Habilidades</h2>
+          
           </div>
           <Row className="wrapper-circle">
-            <div className="circle1">a</div>
-            <div className="circle2">b</div>
-            <div className="circle3">c</div>
+            <div>
+              <div className="circle1">
+                <div className="social1-icon">
+                  <i class="fab fa-js-square"></i>
+                </div>
+              </div>
+              <div className="text-tools">JAVASCRIPT</div>         
+            </div>
+
+            <div>
+              <div className="circle2">
+                <div className="social1-icon">
+                  <i class="fab fa-node-js"></i>
+                </div>
+              </div>
+              <div className="text-tools">NODE JS</div>
+            </div>
+            <div>
+              <div className="circle3">
+                <div className="social1-icon">
+                  <i class="fab fa-react"></i>
+                </div>
+              </div>
+              <div className="text-tools">REACT</div>
+            </div>
           </Row>
+
           <Row className="wrapper-circle">
-            <div className="circle4">d</div>
-            <div className="circle5">e</div>
-            <div className="circle6">f</div>
+            <div>
+              <div className="circle4">
+                <div className="social1-icon">
+                  <i class="fab fa-html5"></i>
+                </div>
+              </div>
+              <div className="text-tools">HTML5</div>
+            </div>
+
+            <div>
+              <div className="circle5">
+                <div className="social1-icon">
+                  <i class="fab fa-css3"></i>
+                </div>
+              </div>
+              <div className="text-tools">CSS3</div>
+            </div>
+            <div>
+              <div className="circle6">
+                <div className="social1-icon">
+                  <i class="fab fa-github-alt"></i>
+                </div>
+              </div>
+              <div className="text-tools">GITHUB</div>
+            </div>
+          </Row>
+
+          <Row className="wrapper-circle">
+            <div>
+              <div className="circle3">
+                <div className="social1-icon">
+                  <i class="fab fa-figma"></i>
+                </div>
+              </div>
+              <div className="text-tools">FIGMA</div>
+            </div>
+
+            <div>
+              <div className="circle2">
+                <div className="social1-icon">
+                  <i class="fab fa-sass"></i>
+                </div>
+              </div>
+              <div className="text-tools">SASS</div>
+            </div>
+
+            <div>
+              <div className="circle1">
+                <div className="social1-icon">
+                  <i class="fab fa-bootstrap"></i>
+                </div>
+              </div>
+              <div className="text-tools">BOOTS</div>
+            </div>
           </Row>
         </Col>
       </Row>

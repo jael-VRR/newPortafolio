ec441a8fbb506ad34cff6bfea55e7a69711ae6c9
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-11 17:30:48 -0500

    modifying color button

--- src/components/BtnContact.jsx
+++ src/components/BtnContact.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import "../styles/btn.css";
 
 function BtnContact() {
   return (
     <div className="btn">
-      <Button>
+      <button className="button-main">
         <a
-          
           href="https://drive.google.com/file/d/11azypBy1lPKw4ZzdWhGc793nOVDDImy2/view?usp=sharing"
           download="cv-jael-rr"
         >
-          Let's talk!
+          Download cv!
         </a>
-      </Button>
+      </button>
     </div>
   );
 }

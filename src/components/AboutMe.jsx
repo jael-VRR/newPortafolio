c6d782ce879512b6d804e87dfb9a2f7efbb31e5d
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-11 22:48:13 -0500

    create nex section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import "../styles/aboutMe.css";
 
 function AboutMe() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Row>
+        <Col className="info-section">
+          <div className="wrapper-info">
+						<div className="info-title">
+            <h2>Perfil profesional</h2>
+						</div>
+						<div className="wrapper-text">
+						<p classname="text-info">We’ve been working with the teams around the world.</p>
+				    </div>
+          </div>
+					
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default AboutMe
+export default AboutMe;

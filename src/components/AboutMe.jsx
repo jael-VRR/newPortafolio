b661125b2d86379115e31e0fd5fe1d013210dfcb
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-12 12:01:31 -0500

    change size icon

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,14 +8,26 @@ function AboutMe() {
       <Row>
         <Col className="info-section">
           <div className="wrapper-info">
-						<div className="info-title">
-            <h2>Perfil profesional</h2>
-						</div>
-						<div className="wrapper-text">
-						<p classname="text-info">We’ve been working with the teams around the world.</p>
-				    </div>
+            <div className="info-title">
+              <h2>Perfil profesional</h2>
+            </div>
+            <div className="wrapper-text">
+              <p className="text-info">
+                Tengo más de una década de experiencia en proyectos digitales
+                trabajando para grandes y pequeñas empresas. Diseño estrategias
+                de contenidos e interacciones claves en productos y servicios
+                digitales; creo los prototipos para probar esos diseños y
+                estrategias con usuarios y también desarrollo sitios web en base
+                a lo anterior. Utilizo Design Thinking, Lean Startup y
+                Metodologías Ágiles en mis proyectos para optimizar el proceso
+                de innovación. Mi formación profesional es de Administradora,
+                 y mi especialización en el mundo de la
+                tecnología es Front End Developer egresada de Laboratoria
+                . Me encanta
+                aprender, innovar, codear y viajar. Vivo en Lima, Perú..
+              </p>
+            </div>
           </div>
-					
         </Col>
       </Row>
     </div>

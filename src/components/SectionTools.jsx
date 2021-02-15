a9f331c24a321bae02572a4df3e3bd8de2283663
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-14 22:31:53 -0500

    modified tools

--- src/components/SectionTools.jsx
+++ src/components/SectionTools.jsx
@@ -2,7 +2,6 @@ import { Fragment } from "react";
 import { Row, Col } from "react-bootstrap";
 import "../styles/sectionTools.css";
 
-
 function SectionTools() {
   return (
     <Fragment>
@@ -11,18 +10,54 @@ function SectionTools() {
           <div className="container-text">
             <h2>Habilidades</h2>
           </div>
-          <Row className="wrapper-circle">
-            <div className="icon-mask">
-   
 
-           
+          <Row className="wrapper-circle">
+          <div className="wrapper-square">
+        <div className="square">
+
+          
+          <svg height="0" width="0">
+            <defs>
+              <clipPath id="svgPath">
+                <path
+                  id="uiRect1"
+                  d="M86,83 L88,83 L88,81 L86,81 L86,83 Z M90,79 L90,85 L84,85 L84,79 L90,79 Z"
+                  fill="#FFFFFF"
+                  transform="translate(87.000000, 82.000000) rotate(-315.000000) translate(-87.000000, -82.000000) "
+                ></path>
+                <path
+                  id="uiRect2"
+                  d="M92,122 L93,122 L93,121 L92,121 L92,122 Z M95,119 L95,124 L90,124 L90,119 L95,119 Z"
+                  fill="#FFFFFF"
+                  transform="translate(92.500000, 121.500000) rotate(-315.000000) translate(-92.500000, -121.500000) "
+                ></path>
+                <path
+                  id="uiRect3"
+                  d="M125,108 L126,108 L126,107 L125,107 L125,108 Z M128,105 L128,110 L123,110 L123,105 L128,105 Z"
+                  fill="#FFFFFF"
+                  transform="translate(125.500000, 107.500000) rotate(-315.000000) translate(-125.500000, -107.500000) "
+                ></path>
+                <path
+                  id="uiRect4"
+                  d="M86,83 L88,83 L88,81 L86,81 L86,83 Z M90,79 L90,85 L84,85 L84,79 L90,79 Z"
+                  fill="#FFFFFF"
+                  transform="translate(87.000000, 82.000000) rotate(-315.000000) translate(-87.000000, -82.000000) "
+                ></path>
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+      </div>
+
+
+
+            <div className="icon-mask">
               <div className="circle1">
                 <div className="social1-icon">
-                
                   <i className="fab fa-js-square"></i>
                 </div>
               </div>
-              <div className="text-tools">JAVASCRIPT</div>         
+              <div className="text-tools">JAVASCRIPT</div>
             </div>
 
             <div>
@@ -34,6 +69,9 @@ function SectionTools() {
               <div className="text-tools">NODE JS</div>
             </div>
             <div>
+
+
+
               <div className="circle3">
                 <div className="social1-icon">
                   <i className="fab fa-react"></i>
@@ -43,8 +81,14 @@ function SectionTools() {
             </div>
           </Row>
 
+
+
+
           <Row className="wrapper-circle">
             <div>
+
+
+
               <div className="circle4">
                 <div className="social1-icon">
                   <i className="fab fa-html5"></i>
@@ -52,6 +96,8 @@ function SectionTools() {
               </div>
               <div className="text-tools">HTML5</div>
             </div>
+
+
 
             <div>
               <div className="circle5">
@@ -62,6 +108,8 @@ function SectionTools() {
               <div className="text-tools">CSS3</div>
             </div>
             <div>
+
+
               <div className="circle6">
                 <div className="social1-icon">
                   <i className="fab fa-github-alt"></i>

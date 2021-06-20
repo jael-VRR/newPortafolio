0f3eb2e80d4c406a6d050eb8bd05abdbd7fffca8
jael-VRR <jrodriguezruidias@gmail.com>
2021-06-19 19:15:32 -0500

    delete svg

--- src/components/SectionTools.jsx
+++ src/components/SectionTools.jsx
@@ -12,42 +12,7 @@ function SectionTools() {
           </div>
 
           <Row className="wrapper-circle">
-          <div className="wrapper-square">
-        <div className="square">
-
-          
-          <svg height="200px" width="200px">
-            <defs>
-              <clipPath id="svgPath">
-                <path
-                  id="uiRect1"
-                  d="M86,83 L88,83 L88,81 L86,81 L86,83 Z M90,79 L90,85 L84,85 L84,79 L90,79 Z"
-                  fill="#FFFFFF"
-                  transform="translate(87.000000, 82.000000) rotate(-315.000000) translate(-87.000000, -82.000000) "
-                ></path>
-                <path
-                  id="uiRect2"
-                  d="M92,122 L93,122 L93,121 L92,121 L92,122 Z M95,119 L95,124 L90,124 L90,119 L95,119 Z"
-                  fill="#FFFFFF"
-                  transform="translate(92.500000, 121.500000) rotate(-315.000000) translate(-92.500000, -121.500000) "
-                ></path>
-                <path
-                  id="uiRect3"
-                  d="M125,108 L126,108 L126,107 L125,107 L125,108 Z M128,105 L128,110 L123,110 L123,105 L128,105 Z"
-                  fill="#FFFFFF"
-                  transform="translate(125.500000, 107.500000) rotate(-315.000000) translate(-125.500000, -107.500000) "
-                ></path>
-                <path
-                  id="uiRect4"
-                  d="M86,83 L88,83 L88,81 L86,81 L86,83 Z M90,79 L90,85 L84,85 L84,79 L90,79 Z"
-                  fill="#FFFFFF"
-                  transform="translate(87.000000, 82.000000) rotate(-315.000000) translate(-87.000000, -82.000000) "
-                ></path>
-              </clipPath>
-            </defs>
-          </svg>
-        </div>
-      </div>
+           
 
 
 
